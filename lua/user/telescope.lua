060da582e939c15a73987b12c81591b4390a0512
Vasily Khoruzhick <anarsoul@gmail.com>
2022-11-22 14:13:56 -0800

    Change telescope key bindings

--- lua/user/telescope.lua
+++ lua/user/telescope.lua
@@ -17,8 +17,8 @@ telescope.setup {
       i = {
         ["<Down>"] = actions.cycle_history_next,
         ["<Up>"] = actions.cycle_history_prev,
-        ["<C-j>"] = actions.move_selection_next,
-        ["<C-k>"] = actions.move_selection_previous,
+        ["<S-Right>"] = actions.move_selection_next,
+        ["<S-Left>"] = actions.move_selection_previous,
       },
     },
   },
