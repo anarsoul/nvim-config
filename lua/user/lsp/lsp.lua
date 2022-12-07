786cc496eec997fca9e440b1c289e0e4578658aa
Vasily Khoruzhick <anarsoul@gmail.com>
2022-12-07 13:22:14 -0800

    Add lsp and cmp

---
+++ lua/user/lsp/lsp.lua
@@ -0,0 +1,42 @@
+local servers = {
+	"pyright",
+	"rust_analyzer",
+	"clangd",
+}
+
+local settings = {
+	ui = {
+		border = "none",
+		icons = {
+			package_installed = "◍",
+			package_pending = "◍",
+			package_uninstalled = "◍",
+		},
+	},
+	log_level = vim.log.levels.INFO,
+	max_concurrent_installers = 4,
+}
+
+local lspconfig_status_ok, lspconfig = pcall(require, "lspconfig")
+if not lspconfig_status_ok then
+	return
+end
+
+local opts = {}
+
+for _, server in pairs(servers) do
+	opts = {
+		on_attach = require("user.lsp.handlers").on_attach,
+		capabilities = require("user.lsp.handlers").capabilities,
+	}
+
+	server = vim.split(server, "@")[1]
+
+	pcall(require, "user.lsp.settings." .. server)
+	if server == "rust_analyzer" then
+		goto continue
+	end
+
+	lspconfig[server].setup(opts)
+	::continue::
+end
--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -60,19 +60,19 @@ return packer.startup(function(use)
   use { "lunarvim/darkplus.nvim", commit = "d308e95" }
 
   -- cmp plugins
-  --use { "hrsh7th/nvim-cmp", commit = "df6734aa018d6feb4d76ba6bda94b1aeac2b378a" } -- The completion plugin
-  --use { "hrsh7th/cmp-buffer", commit = "62fc67a2b0205136bc3e312664624ba2ab4a9323" } -- buffer completions
-  --use { "hrsh7th/cmp-path", commit = "466b6b8270f7ba89abd59f402c73f63c7331ff6e" } -- path completions
-  --use { "saadparwaiz1/cmp_luasnip", commit = "a9de941bcbda508d0a45d28ae366bb3f08db2e36" } -- snippet completions
-  --use { "hrsh7th/cmp-nvim-lsp", commit = "affe808a5c56b71630f17aa7c38e15c59fd648a8" }
+  use { "hrsh7th/nvim-cmp", commit = "93f385c" } -- The completion plugin
+  use { "hrsh7th/cmp-buffer", commit = "3022dbc" } -- buffer completions
+  use { "hrsh7th/cmp-path", commit = "91ff86c" } -- path completions
+  -- use { "saadparwaiz1/cmp_luasnip", commit = "1809552" } -- snippet completions
+  use { "hrsh7th/cmp-nvim-lsp", comcmit = "5922477" }
   --use { "hrsh7th/cmp-nvim-lua", commit = "d276254e7198ab7d00f117e88e223b4bd8c02d21" }
 
-  ---- LSP
-  --use { "neovim/nvim-lspconfig", commit = "148c99bd09b44cf3605151a06869f6b4d4c24455" } -- enable LSP
+  -- LSP
+  use { "neovim/nvim-lspconfig", commit = "c720632" } -- enable LSP
   --use { "williamboman/nvim-lsp-installer", commit = "e9f13d7acaa60aff91c58b923002228668c8c9e6" } -- simple to use language server installer
   --use { "jose-elias-alvarez/null-ls.nvim", commit = "ff40739e5be6581899b43385997e39eecdbf9465" } -- for formatters and linters
   --use { "RRethy/vim-illuminate", commit = "c82e6d04f27a41d7fdcad9be0bce5bb59fcb78e5" }
-  --use { "simrat39/rust-tools.nvim", commit = "11dcd674781ba68a951ab4c7b740553cae8fe671"}
+  use { "simrat39/rust-tools.nvim", commit = "99fd123"}
 
   -- Telescope
   use { "nvim-telescope/telescope.nvim", commit = "7a4ffef" }
